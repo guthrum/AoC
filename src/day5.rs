570bfda8bb937043796f84d8bc67e526d48203f4
Tim Armstrong <armstrongtimmy@gmail.com>
2019-12-07 11:17:37 +0000

    finish day 5 part 2.

--- src/day5.rs
+++ src/day5.rs
@@ -12,11 +12,15 @@ enum AddressingMode {
 
 #[derive(Copy, Clone, Debug)]
 enum Command {
-    END(),
-    ADD(AddressingMode, AddressingMode, AddressingMode),
-    MULTIPLY(AddressingMode, AddressingMode, AddressingMode),
-    INPUT(usize),
-    OUTPUT(usize),
+    End(),
+    Add(AddressingMode, AddressingMode, AddressingMode),
+    Multiply(AddressingMode, AddressingMode, AddressingMode),
+    JmpIfTrue(AddressingMode, AddressingMode),
+    JmpIfFalse(AddressingMode, AddressingMode),
+    LessThan(AddressingMode, AddressingMode, AddressingMode),
+    Equal(AddressingMode, AddressingMode, AddressingMode),
+    IoRead(usize),
+    IoWrite(usize),
 }
 
 fn read_file(path: &str) -> std::io::Result<Vec<i32>> {
@@ -43,7 +47,6 @@ impl Machine {
             instruc_str = format!("{}{}", String::from_utf8(
                 vec![b'0'; INSTRUCTION_LENGTH - instruc_str.len()]).expect("failed to create padding string"), instruc_str);
         }
-        // println!("op str = {}", instruc_str);
         let read_mode_3: usize = usize::try_from(instruc_str.remove(0).to_digit(10)?).ok()?;
         let read_mode_2: usize = usize::try_from(instruc_str.remove(0).to_digit(10)?).ok()?;
         let read_mode_1: usize = usize::try_from(instruc_str.remove(0).to_digit(10)?).ok()?;
@@ -62,34 +65,50 @@ impl Machine {
 
     fn _parse_slice(slice: &[i32]) -> Option<(Command, usize)> {
         let op_vec = Self::_generate_operation_vec(slice[0])?;
-        // println!("op vec = {:?}", op_vec);
         match op_vec.0 {
-            1 => Some((Command::ADD(
+            1 => Some((Command::Add(
                 Self::_create_addressing_mode(op_vec.1, slice[1]),
                 Self::_create_addressing_mode(op_vec.2, slice[2]),
                 Self::_create_addressing_mode(op_vec.3, slice[3]),
             ), 4)),
-            2 => Some((Command::MULTIPLY(
+            2 => Some((Command::Multiply(
                 Self::_create_addressing_mode(op_vec.1, slice[1]),
                 Self::_create_addressing_mode(op_vec.2, slice[2]),
                 Self::_create_addressing_mode(op_vec.3, slice[3]),
             ), 4)),
-            3 => Some((Command::INPUT(slice[1] as usize), 2)),
-            4 => Some((Command::OUTPUT(slice[1] as usize), 2)),
-            99 => Some((Command::END(), 1)),
+            3 => Some((Command::IoRead(slice[1] as usize), 2)),
+            4 => Some((Command::IoWrite(slice[1] as usize), 2)),
+            5 => Some((Command::JmpIfTrue(
+                Self::_create_addressing_mode(op_vec.1, slice[1]),
+                Self::_create_addressing_mode(op_vec.2, slice[2]),
+            ), 3)),
+            6 => Some((Command::JmpIfFalse(
+                Self::_create_addressing_mode(op_vec.1, slice[1]),
+                Self::_create_addressing_mode(op_vec.2, slice[2]),
+            ), 3)),
+            7 => Some((Command::LessThan(
+                Self::_create_addressing_mode(op_vec.1, slice[1]),
+                Self::_create_addressing_mode(op_vec.2, slice[2]),
+                Self::_create_addressing_mode(op_vec.3, slice[3]),
+            ), 4)),
+            8 => Some((Command::Equal(
+                Self::_create_addressing_mode(op_vec.1, slice[1]),
+                Self::_create_addressing_mode(op_vec.2, slice[2]),
+                Self::_create_addressing_mode(op_vec.3, slice[3]),
+            ), 4)),
+            99 => Some((Command::End(), 1)),
             _ => None
         }
     }
 
-    fn read_memory(&self, addressing_mode: AddressingMode)-> i32 {
+    fn _read_memory(&self, addressing_mode: AddressingMode)-> i32 {
         match addressing_mode {
             AddressingMode::Immediate(value) => value,
             AddressingMode::Register(pos) => self.state[pos],
         }
     }
 
-    fn write_memory(&mut self, addressing_mode: AddressingMode, value: i32) {
-        // println!("{:?} {}", addressing_mode, value);
+    fn _write_memory(&mut self, addressing_mode: AddressingMode, value: i32) {
         match addressing_mode {
             AddressingMode::Immediate(_) => panic!("can't write value."),
             AddressingMode::Register(pos) => self.state[pos] = value,
@@ -108,22 +127,57 @@ impl Machine {
         println!("output {}={:?}", pos, self.state.get(pos));
     }
 
+    fn _two_arg_test(&self, arg1_mode: AddressingMode, arg2_mode: AddressingMode, test: impl Fn(i32, i32) -> bool) -> bool {
+        let arg1 = self._read_memory(arg1_mode);
+        let arg2 = self._read_memory(arg2_mode);
+        test(arg1, arg2)
+    }
+
     fn run_machine(&mut self) -> i32 {
         let mut program_counter = 0;
         let mut parsed_command = Self::_parse_slice(&self.state[program_counter .. program_counter+4]);
         while let Some(command) = parsed_command {
             match command.0 {
-                Command::END() => return self.state[0],
-                Command::ADD(v1, v2, res) => {
-                    self.write_memory(res, self.read_memory(v1) + self.read_memory(v2))
+                Command::End() => return self.state[0],
+                Command::Add(v1, v2, res) => {
+                    self._write_memory(res, self._read_memory(v1) + self._read_memory(v2));
+                    program_counter += command.1;
                 },
-                Command::MULTIPLY(v1, v2, res) => {
-                    self.write_memory(res, self.read_memory(v1) * self.read_memory(v2))
+                Command::Multiply(v1, v2, res) => {
+                    self._write_memory(res, self._read_memory(v1) * self._read_memory(v2));
+                    program_counter += command.1;
                 },
-                Command::INPUT(pos) => self._read_input(pos),
-                Command::OUTPUT(pos) => self._write_output(pos),
+                Command::LessThan(arg1, arg2, res) => {
+                    self._write_memory(res, self._two_arg_test(arg1, arg2,|v1, v2| -> bool { v1 < v2 }) as i32);
+                    program_counter += command.1;
+                },
+                Command::Equal(arg1, arg2, res) => {
+                    self._write_memory(res, self._two_arg_test(arg1, arg2,|v1, v2| -> bool { v1 == v2 }) as i32);
+                    program_counter += command.1;
+                },
+                Command::IoRead(pos) => {
+                    self._read_input(pos);
+                    program_counter += command.1;
+                },
+                Command::IoWrite(pos) => {
+                    self._write_output(pos);
+                    program_counter += command.1;
+                },
+                Command::JmpIfTrue(test, ptr) => {
+                    if self._read_memory(test) != 0 { 
+                        program_counter = self._read_memory(ptr) as usize
+                    } else {
+                        program_counter += command.1;
+                    }
+                },
+                Command::JmpIfFalse(test, ptr) => {
+                    if self._read_memory(test) == 0 { 
+                        program_counter = self._read_memory(ptr) as usize
+                    } else {
+                        program_counter += command.1;
+                    }
+                }
             }
-            program_counter += command.1;
             parsed_command = Self::_parse_slice(&self.state[program_counter .. std::cmp::min(program_counter+4, self.state.len())]);
         }
         0
