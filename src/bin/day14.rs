c4203da9497df4b2fbaeed39256d630ea988a154
Tim Armstrong <armstrongtimmy@gmail.com>
2020-01-05 17:58:10 +0000

    day 14 part 2 complete.

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+use std::collections::BTreeMap;
 use std::collections::{HashMap, VecDeque};
 use std::convert::TryFrom;
 
@@ -20,7 +22,6 @@ struct Formula {
     requirements: Vec<Component>,
     result: Component,
 }
-
 
 impl TryFrom<&str> for Formula {
     type Error = String;
@@ -122,8 +123,51 @@ impl TryFrom<&str> for RecipeGraph {
             .map(|line| Formula::try_from(line).unwrap())
             .map(|formula| (formula.result.name.clone(), formula))
             .collect();
-        Ok(RecipeGraph { formulas, extra_resources: HashMap::new() })
+        Ok(RecipeGraph {
+            formulas,
+            extra_resources: HashMap::new(),
+        })
     }
+}
+
+fn part2(input: &str) -> Option<(i64, i64)> {
+    let total_ore: i64 = 1_000_000_000_000;
+
+    // cost_per_f, made, ore remaining
+    let mut last_trial = {
+        let mut rg = RecipeGraph::try_from(input).unwrap();
+        let ore_per_fuel = rg.required_ore_for_single_fuel()? as i64;
+        (ore_per_fuel, 1, total_ore - ore_per_fuel)
+    };
+    // cost_per_f, made, ore remaining
+    let mut best_trial = last_trial;
+    let mut trial_count = 0;
+    loop {
+        // exit condition
+        if last_trial.2 < 0 && (last_trial.1 - best_trial.1) == 1 {
+            break;
+        }
+        trial_count += 1;
+
+        let test_value = match last_trial.2.cmp(&0) {
+            Ordering::Less => best_trial.1 + ((last_trial.1 - best_trial.1) / 2),
+            _ => {
+                let made_with_excess = last_trial.2 / last_trial.0;
+                made_with_excess + last_trial.1
+            }
+        };
+        let mut rg = RecipeGraph::try_from(input).unwrap();
+        let ore_used = rg.require_ore_for_fuel(test_value as usize)? as i64;
+        let ore_left = total_ore - ore_used;
+        let ore_per_fuel = ore_used / test_value;
+
+        last_trial = (ore_per_fuel, test_value, ore_left);
+        if ore_left >= 0 && ore_left < best_trial.2 {
+            best_trial = last_trial.clone();
+        }
+    }
+
+    Some((trial_count, best_trial.1))
 }
 
 fn main() {
@@ -184,10 +228,72 @@ fn main() {
         1 SMHJH => 7 MVGLF
         3 NDQNH => 1 GNSZ
         153 ORE => 9 NFTPZ
-        14 MCHV, 4 JNJHW, 2 DJSP => 4 MWFBW"
+        14 MCHV, 4 JNJHW, 2 DJSP => 4 MWFBW",
     )
     .expect("failed to create graph");
-    println!("{:?}", g.required_ore_for_single_fuel());
+    println!("part 1 = {:?}", g.required_ore_for_single_fuel());
+    println!(
+        "part 2 = {:?}",
+        part2(
+            "5 HLJD, 1 QHSZD, 13 SKZX => 8 MQPH
+        10 LSLV => 4 JNJHW
+        1 MQGF, 4 ZWXDQ, 1 GNSZ => 9 DGDH
+        1 SKZX, 3 DJSP => 1 MCHV
+        6 TWSR, 10 ZHDFS, 10 LQZXQ => 9 LXQNX
+        1 FRVW, 1 CJTW => 9 BRCB
+        20 ZHVNP => 8 XMXL
+        7 JQJXP => 1 ZGZDW
+        13 KRCM => 6 KXPQ
+        4 ZWXDQ, 4 KFKQF, 1 DZDX => 2 MQGF
+        8 DZDX, 2 ZKGM => 3 KFKQF
+        3 FXFTB => 8 KVDGP
+        10 MVGLF, 3 MWFBW, 13 XMXL, 1 CJTW, 2 ZSXJZ, 2 TNCZH, 3 MPFKN, 6 LXQNX => 2 MZMZQ
+        5 FRVW => 3 NWBTP
+        1 MVGLF, 2 NLXD, 6 KVDGP, 2 MQPH, 4 FXTJ, 10 TKXKF, 2 FRWV => 2 CSNS
+        13 TWSR => 9 BNWT
+        2 KRCM => 7 LSLV
+        1 ZHDFS, 11 NTVZD, 1 JQJXP => 6 ZHVNP
+        2 MCHV, 1 JNJHW => 6 NDQNH
+        32 SMHJH, 6 KXPQ => 1 CJTW
+        15 FXFTB, 1 MVGLF => 9 MPFKN
+        119 ORE => 9 KRCM
+        3 TNCZH => 9 BFQLT
+        5 MPFKN, 7 TKXKF, 6 JQJXP, 2 DZDX, 16 LCQJ, 4 DGDH, 4 ZGZDW => 7 WVXW
+        1 ZHDFS, 1 LXQNX => 3 TNCZH
+        4 ZMVKM, 1 BRQT => 3 QHSZD
+        24 FRVW, 1 KVDGP, 2 ZLNM => 3 FGLNK
+        2 KXPQ, 1 LSLV, 22 HNRQ => 5 ZWXDQ
+        6 ZWXDQ => 1 FRVW
+        1 FXFTB, 2 MWFBW => 6 ZHDFS
+        32 FRVW => 5 FRWV
+        6 FXFTB, 6 NDQNH, 2 MWFBW => 1 JQJXP
+        9 ZMVKM, 6 QHSZD, 5 LSLV => 4 SMHJH
+        3 CHKZ => 6 HLJD
+        21 BFQLT => 6 FXTJ
+        1 SMHJH, 4 FXFTB => 6 CHKZ
+        13 FRVW, 13 JQJXP, 1 GNSZ => 8 ZSXJZ
+        2 NDQNH => 8 NTVZD
+        3 KRCM => 2 ZKGM
+        13 ZHDFS, 14 ZWXDQ, 1 CHKZ => 7 LQZXQ
+        2 BNWT, 3 CHKZ => 7 ZLNM
+        167 ORE => 1 BRQT
+        1 LSLV => 3 DZDX
+        8 MZMZQ, 7 NWBTP, 3 WVXW, 44 MQPH, 3 DJSP, 1 CSNS, 3 BRCB, 32 LQZXQ => 1 FUEL
+        8 ZLNM => 2 NLXD
+        30 JQJXP, 9 FGLNK => 7 LCQJ
+        1 ZKGM, 19 KXPQ => 8 DJSP
+        4 DJSP => 6 FXFTB
+        25 NFTPZ => 6 ZMVKM
+        14 ZHVNP, 1 MVGLF => 9 TKXKF
+        1 BRQT => 2 SKZX
+        6 ZKGM => 7 HNRQ
+        3 DZDX => 5 TWSR
+        1 SMHJH => 7 MVGLF
+        3 NDQNH => 1 GNSZ
+        153 ORE => 9 NFTPZ
+        14 MCHV, 4 JNJHW, 2 DJSP => 4 MWFBW"
+        )
+    );
 }
 
 #[cfg(test)]
