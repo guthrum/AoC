bb4d231ebcb94ed7f7099f52ff2543e5aa4fc622
Tim Armstrong <git.tim.armstrong@gmail.com>
2021-12-05 12:09:26 +0000

    Reduce memory allocations slightly.

--- aoc-2021/src/day5.rs
+++ aoc-2021/src/day5.rs
@@ -1,38 +1,36 @@
 use std::{collections::HashMap, fs::read_to_string};
 
-fn parse(input: &str) -> Vec<((i32, i32), (i32, i32))> {
-    input.lines().map(|l| {
-        l
-            .split(" -> ")
-            .map(|p| {
-                p.split(",")
-                    .map(|c| i32::from_str_radix(c, 10).unwrap())
-                    .collect::<Vec<i32>>()
-            })
-            .collect::<Vec<Vec<i32>>>()
-    })
-    .map(|input| {
-        ((input[0][0], input[0][1]), (input[1][0], input[1][1]))
-    })
-    .collect()
+fn read_coords(p: &str) -> (i32, i32) {
+    let mut parts = p.split(",");
+    (
+        i32::from_str_radix(parts.next().unwrap(), 10).unwrap(),
+        i32::from_str_radix(parts.next().unwrap(), 10).unwrap(),
+    )
 }
 
-fn points_on_line(p1: (i32, i32), p2: (i32, i32)) -> Vec<(i32, i32)> {
+fn parse(input: &str) -> Vec<((i32, i32), (i32, i32))> {
+    let mut res = Vec::with_capacity(1000);
+    res.extend(input
+        .lines()
+        .map(|l| {
+            let mut coords = l.split(" -> ");
+            (read_coords(coords.next().unwrap()), read_coords(coords.next().unwrap()))
+        }));
+    res
+}
+
+fn points_on_line(p1: (i32, i32), p2: (i32, i32)) -> Box<dyn Iterator<Item = (i32, i32)>> {
     if p1.0 == p2.0 {
         if p1.1 > p2.1 {
             points_on_line(p2, p1)
         } else {
-            (p1.1..=p2.1).into_iter()
-                .map(|y| (p1.0, y))
-                .collect()
+            Box::new((p1.1..=p2.1).into_iter().map(move |y| (p1.0, y)))
         }
     } else if p1.1 == p2.1 {
         if p1.0 > p2.0 {
             points_on_line(p2, p1)
         } else {
-            (p1.0..=p2.0).into_iter()
-                .map(|x| (x, p1.1))
-                .collect()
+            Box::new((p1.0..=p2.0).into_iter().map(move |x| (x, p1.1)))
         }
     } else {
         let dx = if p1.0 < p2.0 { 1 } else { -1 };
@@ -44,15 +42,16 @@ fn points_on_line(p1: (i32, i32), p2: (i32, i32)) -> Vec<(i32, i32)> {
             p = (p.0 + dx, p.1 + dy);
             res.push(p)
         }
-        res
+        Box::new(res.into_iter())
     }
 }
 
 fn solve(input: &str) -> (usize, usize) {
     let line_sections = parse(input);
     let mut line_sections_part_1: HashMap<(i32, i32), i32> = HashMap::new();
-    line_sections.iter()
-        .filter(|v| v.0.0 == v.1.0 || v.0.1 == v.1.1)
+    line_sections
+        .iter()
+        .filter(|v| v.0 .0 == v.1 .0 || v.0 .1 == v.1 .1)
         .cloned()
         .flat_map(|(p1, p2)| points_on_line(p1, p2).into_iter())
         .for_each(|p| {
@@ -62,7 +61,8 @@ fn solve(input: &str) -> (usize, usize) {
     let res1 = line_sections_part_1.values().filter(|v| **v > 1).count();
 
     let mut line_sections_part_2: HashMap<(i32, i32), i32> = HashMap::new();
-    line_sections.into_iter()
+    line_sections
+        .into_iter()
         .flat_map(|(p1, p2)| points_on_line(p1, p2).into_iter())
         .for_each(|p| {
             let count = line_sections_part_2.entry(p).or_insert(0);
