94c12b2fb61087c4183cd38654e1b9c2ba4dba6d
Tim Armstrong <git.tim.armstrong@gmail.com>
2021-12-11 21:57:31 +0000

    Optimize day6.

--- aoc-2021/src/day6.rs
+++ aoc-2021/src/day6.rs
@@ -1,41 +1,19 @@
 use std::fs::read_to_string;
 
-fn calculate_number(i: u8, days: u32) -> (u8, usize) {
-    let mut state = Vec::with_capacity(6703087164);
-    state.push(i);
-    for _i in 1..=days {
-        let mut append = Vec::with_capacity(state.len() / 2);
-        for v in &mut state {
-            if *v == 0 {
-                *v = 6;
-                append.push(8);
-            } else {
-                *v -= 1;
-            }
-        }
-        state.append(&mut append);
+fn solve(input: &str, days: usize) -> usize {
+    let mut fish_life_span_counts = vec![0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for v in input.lines().next().unwrap().split(",") {
+        let idx = usize::from_str_radix(v, 10).unwrap();
+        fish_life_span_counts[idx] += 1;
     }
 
-    (i, state.len())
-}
-
-fn solve(input: &str, days: u32) -> usize {
-    let mut counts = [0 as usize; 9];
-    for i in 0..=8 {
-        let (_, val) = calculate_number(i, days);
-        counts[i as usize] = val;
-        println!("\t{} for {} days = {}", i, days, val);
+    for _day in 0..days {
+        let day_0 = fish_life_span_counts.remove(0);
+        fish_life_span_counts[6] += day_0;
+        fish_life_span_counts.push(day_0);
     }
-    println!("calculations complete");
 
-    input
-        .lines()
-        .next()
-        .unwrap()
-        .split(",")
-        .map(|v| u8::from_str_radix(v, 10).expect(&format!("{} is invalid", v)))
-        .map(|v| counts[v as usize])
-        .sum()
+    fish_life_span_counts.iter().sum()
 }
 
 fn main() {
